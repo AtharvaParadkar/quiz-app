df38cfce24b5d490cfd6a765e218802dfc7ffa76
AtharvaParadkar <atharvaparadkar91@gmail.com>
2024-01-29 23:17:11 +0530

    Dynamic Buttons

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -11,27 +13,48 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
-    return Column(
-      children: [
-        const Text('The question...'),
-        const SizedBox(height: 30),
-        ElevatedButton(
-          onPressed: () {},
-          child: const Text('Answer 1'),
-        ),
-        ElevatedButton(
-          onPressed: () {},
-          child: const Text('Answer 2'),
-        ),
-        ElevatedButton(
-          onPressed: () {},
-          child: const Text('Answer 3'),
-        ),
-        ElevatedButton(
-          onPressed: () {},
-          child: const Text('Answer 4'),
-        ),
-      ],
+    final currentQuestion = questions[0];
+
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            currentQuestion.text,
+            style: const TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(height: 30),
+          //? the '...'(spread) operator is used for spreading elements of an iterable ( a list)
+          //? or for spreading the key-value pairs of a map.
+          //? It's a concise way to include the elements of one collection into another.
+          //* Dynamically generated buttons
+          ...currentQuestion.answers.map(
+            (answer) {
+              return AnswerButton(answerText: answer, onTap: () {});
+            },
+          ),
+          //* Hard Coded Buttons
+          // AnswerButton(
+          //   answerText: currentQuestion.answers[0],
+          //   onTap: () {},
+          // ),
+          // AnswerButton(
+          //   answerText: currentQuestion.answers[1],
+          //   onTap: () {},
+          // ),
+          // AnswerButton(
+          //   answerText: currentQuestion.answers[2],
+          //   onTap: () {},
+          // ),
+          // AnswerButton(
+          //   answerText: currentQuestion.answers[3],
+          //   onTap: () {},
+          // ),
+        ],
+      ),
     );
   }
 }
