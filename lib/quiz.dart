0b7f120764e9f21143e9fb48995d42ec897b9e77
AtharvaParadkar <atharvaparadkar91@gmail.com>
2024-06-04 16:44:29 +0530

    Removed Debug

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -62,6 +62,7 @@ class _QuizState extends State<Quiz> {
     }
 
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Container(
           decoration: const BoxDecoration(
